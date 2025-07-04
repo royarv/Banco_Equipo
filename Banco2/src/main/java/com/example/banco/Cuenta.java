dacd5cba5f03df4b96e26b97022100061bb98cf1
royarv <20243rd054@utez.edu.mx>
2025-07-04 16:46:14 -0600

    Add files via upload
    Repositorio creado de Banco Trabajado por Rodrigo, Jetzemany, Jesus, Eliel

---
+++ Banco2/src/main/java/com/example/banco/Cuenta.java
@@ -0,0 +1,21 @@
+package com.example.banco;
+
+public class Cuenta {
+    private static double saldo = 0;
+
+    public static double getSaldo() {
+        return saldo;
+    }
+
+    public static void ingresar(double cantidad) {
+        saldo += cantidad;
+    }
+
+    public static boolean retirar(double cantidad) {
+        if (cantidad <= saldo) {
+            saldo -= cantidad;
+            return true;
+        }
+        return false;
+    }
+}
---
+++ Banco2/src/main/java/com/example/banco/Main.java
@@ -0,0 +1,21 @@
+package com.example.banco;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("login-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Banco - Login");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
